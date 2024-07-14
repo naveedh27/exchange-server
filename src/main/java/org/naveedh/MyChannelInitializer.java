b57de2ca40020df9f0252262808016fcbc91183c
naveedh27 <naveedh27@gmail.com>
2024-07-15 01:44:32 +0530

    Simple Echo

--- src/main/java/org/naveedh/MyChannelInitializer.java
+++ src/main/java/org/naveedh/MyChannelInitializer.java
@@ -1,7 +1,11 @@
 package org.naveedh;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.*;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.util.ReferenceCountUtil;
+
+import java.nio.charset.StandardCharsets;
 
 public class MyChannelInitializer extends ChannelInitializer<SocketChannel> {
 
@@ -10,58 +14,69 @@ public class MyChannelInitializer extends ChannelInitializer<SocketChannel> {
         socketChannel.pipeline().addLast(new ChannelInboundHandler() {
             @Override
             public void handlerAdded(ChannelHandlerContext channelHandlerContext) throws Exception {
-                channelHandlerContext.writeAndFlush("Echo Back");
+                System.out.println("Handler Added");
             }
 
             @Override
             public void handlerRemoved(ChannelHandlerContext channelHandlerContext) throws Exception {
-
+                System.out.println("Handler Removed");
+                channelHandlerContext.writeAndFlush("Close");
             }
 
             @Override
             public void channelRegistered(ChannelHandlerContext channelHandlerContext) throws Exception {
-
+                System.out.println("Handler Registered");
             }
 
             @Override
             public void channelUnregistered(ChannelHandlerContext channelHandlerContext) throws Exception {
-
+                System.out.println("Handler UnRegistered");
             }
 
             @Override
             public void channelActive(ChannelHandlerContext channelHandlerContext) throws Exception {
-
+                System.out.println("Channel Active");
             }
 
             @Override
             public void channelInactive(ChannelHandlerContext channelHandlerContext) throws Exception {
-
+                System.out.println("Channel InActive");
             }
 
             @Override
             public void channelRead(ChannelHandlerContext channelHandlerContext, Object o) throws Exception {
-                channelHandlerContext.writeAndFlush()
-                System.out.println(o.toString());
+                System.out.println("Channel Read");
+                ByteBuf in = (ByteBuf) o;
+                try {
+                    while (in.isReadable()) { // (1)
+                        System.out.print((char) in.readByte());
+                        System.out.flush();
+                    }
+                } finally {
+                    ReferenceCountUtil.release(o); // (2)
+                }
+                channelHandlerContext.flush();
             }
 
             @Override
             public void channelReadComplete(ChannelHandlerContext channelHandlerContext) throws Exception {
-
+                System.out.println("Channel Read Complete");
+                channelHandlerContext.flush();
             }
 
             @Override
             public void userEventTriggered(ChannelHandlerContext channelHandlerContext, Object o) throws Exception {
-
+                System.out.println("userEventTriggered");
             }
 
             @Override
             public void channelWritabilityChanged(ChannelHandlerContext channelHandlerContext) throws Exception {
-
+                System.out.println("channelWritabilityChanged");
             }
 
             @Override
             public void exceptionCaught(ChannelHandlerContext channelHandlerContext, Throwable throwable) throws Exception {
-
+                System.out.println("Exception Caught");
             }
         });
     }
--- src/main/java/org/naveedh/NettyServer.java
+++ src/main/java/org/naveedh/NettyServer.java
@@ -2,36 +2,45 @@ package org.naveedh;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class NettyServer {
 
-    @Autowired
-    MyHandler myHandler;
 
     public static void main(String[] args) throws Exception {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
         MyHandler myHandler = ctx.getBean(MyHandler.class);
 
+
+        // Create the multithreaded event loops for the server
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
 
         try {
-            ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .childHandler(new MyChannelInitializer());
+            // A helper class that simplifies server configuration
+            ServerBootstrap httpBootstrap = new ServerBootstrap();
 
-            ChannelFuture f = b.bind(8080).sync();
-            f.channel().closeFuture().sync();
-        } finally {
-            bossGroup.shutdownGracefully();
+            // Configure the server
+            httpBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new MyChannelInitializer()) // <-- Our handler created here
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            // Bind and start to accept incoming connections.
+            ChannelFuture httpChannel = httpBootstrap.bind(8080).sync();
+
+            // Wait until server socket is closed
+            httpChannel.channel().closeFuture().sync();
+        }
+        finally {
             workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
         }
     }
 }
