4852518ce0aa50c4ec895448b13bd79b9d2d9427
naveedh27 <naveedh27@gmail.com>
2024-07-26 02:26:47 +0530

    Refactoring

--- client/src/main/java/org/naveedh/NettyClient.java
+++ client/src/main/java/org/naveedh/NettyClient.java
@@ -1,14 +1,10 @@
 package org.naveedh;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class NettyClient {
-
-    private static Logger logger = LoggerFactory.getLogger(NettyClient.class);
 
     public static void main(String[] args) throws InterruptedException {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
